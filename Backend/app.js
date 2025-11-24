f42c0e76bff2f974f0ab2a4ca3347460eef249be
MAHESWARAN <eswermahes@gmail.com>
2025-11-24 15:11:01 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -13,7 +13,7 @@ app.use(cors({
 
 app.use(express.json({limit:"50mb"}))
 app.get('/', (req, res) => {
-    console.log('Server Running');
+    res.send('Server Running');
 });
 
 const dotenv=require("dotenv");
